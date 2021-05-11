113576319cc66606357aaf4280845ccae0096de6
satoshi-dev <52643746+satoshi-dev@users.noreply.github.com>
2021-05-11 16:16:24 +0900

    デバッグ

---
+++ UWP_FullTrust_1/FullTrust_dotnet5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FullTrust_dotnet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello Modified World";
+            Console.WriteLine("This process runs at the full privileges of the user and has access to the entire public desktop API surface");
+            Console.WriteLine("\r\nPress any key to exit ...");
+            Console.ReadLine();
+        }
+    }
+}
